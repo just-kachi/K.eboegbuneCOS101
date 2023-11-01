2f623a27e4431475f94687bf7abb3c5802b9fe8a
just-kachi <141348590+just-kachi@users.noreply.github.com>
2023-11-01 18:37:41 +0100

    Add files via upload

---
+++ week-4/pratice-1/target/debug/deps/pratice_1.d
@@ -0,0 +1,5 @@
+C:\Users\EXAMS\Desktop\KACHICOS101\WEEK 4\pratice-1\target\debug\deps\pratice_1.exe: src\main.rs
+
+C:\Users\EXAMS\Desktop\KACHICOS101\WEEK 4\pratice-1\target\debug\deps\pratice_1.d: src\main.rs
+
+src\main.rs:
