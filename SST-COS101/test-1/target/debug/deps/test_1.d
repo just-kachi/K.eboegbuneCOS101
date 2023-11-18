a081618e924a747eeeb18f8b518c2e5aaacb0711
just-kachi <141348590+just-kachi@users.noreply.github.com>
2023-11-18 16:02:43 +0100

    Delete SST-COS101/test-1 directory

--- SST-COS101/test-1/target/debug/deps/test_1.d
+++
@@ -1,5 +0,0 @@
-C:\Users\user\OneDrive\Documents\K.eboegbuneCOS101\SST-COS101\test-1\target\debug\deps\test_1.exe: src\main.rs
-
-C:\Users\user\OneDrive\Documents\K.eboegbuneCOS101\SST-COS101\test-1\target\debug\deps\test_1.d: src\main.rs
-
-src\main.rs:
--- SST-COS101/test-1/target/debug/test-1.d
+++
@@ -1,1 +0,0 @@
-C:\Users\user\OneDrive\Documents\K.eboegbuneCOS101\SST-COS101\test-1\target\debug\test-1.exe: C:\Users\user\OneDrive\Documents\K.eboegbuneCOS101\SST-COS101\test-1\src\main.rs
